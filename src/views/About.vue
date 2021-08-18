4d3f2ab27cebd46a03ef297c771064f2d270a332
Gerry <gerry@gmt.gmt>
2021-08-19 01:42:52 +0300

    Initial commit

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,10 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about pa-6">
+    <h1>About {{ $store.state.appTitle }}</h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta ipsum aspernatur quidem iste sapiente odit eum beatae maxime aliquid tempore, itaque recusandae, numquam molestias eius alias dolores aperiam enim quos.</p>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta ipsum aspernatur quidem iste sapiente odit eum beatae maxime aliquid tempore, itaque recusandae, numquam molestias eius alias dolores aperiam enim quos.</p>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta ipsum aspernatur quidem iste sapiente odit eum beatae maxime aliquid tempore, itaque recusandae, numquam molestias eius alias dolores aperiam enim quos.</p>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta ipsum aspernatur quidem iste sapiente odit eum beatae maxime aliquid tempore, itaque recusandae, numquam molestias eius alias dolores aperiam enim quos.</p>
+
   </div>
 </template>
